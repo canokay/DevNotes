c7809be82c2bf51ce67102d8df8a59ea735a63a8
Can Okay <canokay2014@gmail.com>
2018-04-25 02:23:53 +0300

    Delete master Python

--- mod3-5.py
+++
@@ -1,4 +0,0 @@
-for i in range(1, 20):
-    if i % 3 == 0 or i % 5 == 0:
-        print(i)
-
--- ortalama.py
+++
@@ -1,4 +0,0 @@
-num1 = int(input("birinci sayıyı giriniz :"))
-num2 = int(input("ikinci sayıyı giriniz :"))
-division = (int(num1) + int(num2))/2
-print('{0} / {1} = {2}'.format(num1, num2, division))
